dca67c588514c4e66c3ad2c7b1916d5914001283
Jakub Doczy <jakubdoczy@gmail.com>
2021-12-21 18:54:28 +0100

    Fixes in db

--- db/Up.sql
+++ db/Up.sql
@@ -5,17 +5,17 @@ CREATE TYPE role AS ENUM ('admin', 'user');
 CREATE TYPE video_state AS ENUM ('Processing', 'Published');
 
 CREATE TABLE users (
-  id BIGINT PRIMARY KEY,
+  id BIGSERIAL PRIMARY KEY,
   email TEXT NOT NULL,
-  passwd_hash TEXT NOT NULL,
+  password TEXT NOT NULL,
   user_role role NOT NULL,
   username TEXT NOT NULL,
   description TEXT,
-  picture_url TEXT,
+  picture_url TEXT
 );
 
 CREATE TABLE videos (
-  id BIGINT SERIAL PRIMARY KEY,
+  id BIGSERIAL PRIMARY KEY,
   creator_id INTEGER NOT NULL REFERENCES users(id),
   name TEXT NOT NULL,
   preview_url TEXT NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE videos (
   likes INTEGER NOT NULL,
   dislikes INTEGER NOT NULL,
   creation_time TIMESTAMP NOT NULL,
-  state video_state NOT NULL,
+  state video_state NOT NULL
 );
 
 
