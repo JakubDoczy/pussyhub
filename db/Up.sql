bd07d84df7398b745018168a2429e94ea75b43be
Jakub Doczy <423199@mail.muni.cz>
2021-12-21 13:31:52 +0100

    Changed enum names in db

--- db/Up.sql
+++ db/Up.sql
@@ -1,21 +1,21 @@
 BEGIN TRANSACTION;
 
-CREATE TYPE ROLE AS ENUM ('Admin', 'User');
+CREATE TYPE role AS ENUM ('admin', 'user');
 
-CREATE TYPE VIDEO_STATE AS ENUM ('Processing', 'Published');
+CREATE TYPE video_state AS ENUM ('Processing', 'Published');
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
+  id BIGINT PRIMARY KEY,
   email TEXT NOT NULL,
-  hash TEXT NOT NULL,
-  role ROLE NOT NULL,
+  passwd_hash TEXT NOT NULL,
+  user_role role NOT NULL,
   username TEXT NOT NULL,
   description TEXT,
   picture_url TEXT,
 );
 
 CREATE TABLE videos (
-  id SERIAL PRIMARY KEY,
+  id BIGINT SERIAL PRIMARY KEY,
   creator_id INTEGER NOT NULL REFERENCES users(id),
   name TEXT NOT NULL,
   preview_url TEXT NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE videos (
   likes INTEGER NOT NULL,
   dislikes INTEGER NOT NULL,
   creation_time TIMESTAMP NOT NULL,
-  state VIDEO_STATE NOT NULL,
+  state video_state NOT NULL,
 );
 
 
