0723622aeca25d585dd2968401e79063a93ded8e
David Konečný <david@konecny.eu>
2022-01-07 22:48:07 +0100

    feat: backend init

---
+++ server/src/model/video.rs
@@ -0,0 +1,17 @@
+use chrono::serde::ts_milliseconds;
+use chrono::{DateTime, Utc};
+use ::serde::{Serialize, Deserialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize, sqlx::FromRow)]
+pub struct Video {
+    pub id: i32,
+    pub creator_id: i32,
+    pub name: String,
+    pub preview_url: String,
+    pub video_url: String,
+    pub views: i32,
+    pub likes: i32,
+    pub dislikes: i32,
+    #[serde(with = "ts_milliseconds")]
+    pub created_at: DateTime<Utc>,
+}
