97cadfe750856d74ef128cff48516166226b8045
Jakub Doczy <423199@mail.muni.cz>
2022-01-13 23:10:25 +0100

    Switched library for signing jwt

--- auth-service/src/registration/invitation.rs
+++ auth-service/src/registration/invitation.rs
@@ -1,29 +1,26 @@
-use chrono::Utc;
-use jsonwebtoken::Header;
 use serde::{Deserialize, Serialize};
 
-use shared_lib::token_validation::{TokenValidator, ALGORITHM};
+use anyhow::Error;
 
-use crate::token_issuer::{TokenIssuer, DEFAULT_VALIDITY_DURATION_SEC};
+use shared_lib::token_validation::{TokenValidator};
+
+use crate::token_issuer::TokenIssuer;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct InvitationClaims {
-    pub exp: usize, // expiration date
     pub uid: i64,
 }
 
 pub trait InvitationIssuer {
-    fn sign_invitation(&self, user_id: i64) -> Result<String, jsonwebtoken::errors::Error>;
+    fn sign_invitation(&self, user_id: i64) -> Result<String, Error>;
 }
 
 impl InvitationIssuer for TokenIssuer {
-    fn sign_invitation(&self, user_id: i64) -> Result<String, jsonwebtoken::errors::Error> {
+    fn sign_invitation(&self, user_id: i64) -> Result<String, Error> {
         let body = InvitationClaims {
-            exp: (Utc::now().timestamp() + DEFAULT_VALIDITY_DURATION_SEC) as usize,
             uid: user_id,
         };
-
-        self.encode(&Header::new(ALGORITHM), &body)
+        self.encode(body)
     }
 }
 
@@ -31,15 +28,15 @@ pub trait InvitationValidator {
     fn validate_invitation(
         &self,
         invitation_token: &str,
-    ) -> Result<i64, jsonwebtoken::errors::Error>;
+    ) -> Result<i64, Error>;
 }
 
 impl InvitationValidator for TokenValidator {
     fn validate_invitation(
         &self,
         invitation_token: &str,
-    ) -> Result<i64, jsonwebtoken::errors::Error> {
+    ) -> Result<i64, Error> {
         let result = self.decode::<InvitationClaims>(invitation_token)?;
-        Ok(result.claims.uid)
+        Ok(result.custom.uid)
     }
 }
