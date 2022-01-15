db8a761a8c1367cdcaa47aaf5ee6335cf6765f2b
Otakar Hirs <485661@mail.muni.cz>
2022-01-15 21:55:57 +0100

    registration form validation

--- shared-lib/src/payload/registration.rs
+++ shared-lib/src/payload/registration.rs
@@ -5,13 +5,13 @@ use validator::{Validate, ValidationErrors};
 #[derive(Debug, Validate, Serialize, Deserialize)]
 pub struct UserRegistrationPayload {
     
-    #[validate(email)]
+    #[validate(email(message = "not a valid email"))]
     pub email: String,
     
-    #[validate(length(min = 4, max = 25))]
+    #[validate(length(min = 4, max = 25, message = "4 to 25 characters"))]
     pub username: String,
 
-    #[validate(length(min = 6, max = 255))]
+    #[validate(length(min = 6, max = 255, message = "must be at least 6 characters long"))]
     pub password: String,
 }
 
